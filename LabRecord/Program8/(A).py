d23458088e480ed7c1abb84d292ddf5fd6e21e25
Syed Yasir <yasironline54@gmail.com>
2025-04-10 12:24:51 +0530

    (A).py

---
+++ LabRecord/Program8/(A).py
@@ -0,0 +1,18 @@
+import pandas as pd
+df = pd.read_csv(r"C:\Users\yasir\OneDrive\Desktop\BCA\python\PPDA lab record\1experience.csv")
+
+mean_value = df['YearsExperience'].mean()
+median_value = df['YearsExperience'].median()
+mode_value = df['YearsExperience'].mode()[0] 
+min_value = df['YearsExperience'].min()
+max_value = df['YearsExperience'].max()
+variance_value = df['YearsExperience'].var()
+std_dev_value = df['YearsExperience'].std()
+
+print(f"Mean: {mean_value}")
+print(f"Median: {median_value}")
+print(f"Mode: {mode_value}")
+print(f"Minimum: {min_value}")
+print(f"Maximum: {max_value}")
+print(f"Variance: {variance_value}")
+print(f"Standard Deviation: {std_dev_value}")
